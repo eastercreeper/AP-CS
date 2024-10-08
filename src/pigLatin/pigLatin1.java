f57f85fbf2862c345f8176c725ee1bc35aa48ee9
eastercreeper <creeperboy889@gmail.com>
2024-10-08 11:04:07 -0400

    im stupid as fuck kill me please

--- src/pigLatin/pigLatin1.java
+++ src/pigLatin/pigLatin1.java
@@ -21,6 +21,19 @@ public class pigLatin1 {
 		return -1;
 	}
 
+	public static boolean checkQU(String s) {
+		for(int i = 0; i < s.length(); i++) {
+			if(isVowel(s.charAt(i)) && s.toLowerCase().charAt(i) == 'u' && s.toLowerCase().charAt(i - 1) == 'q') {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static boolean checkY(String s) {
+		return false;
+	}
+
 	public static boolean checkCaptial(String s) {
         return Character.isUpperCase(s.charAt(0));
 	}
@@ -28,15 +41,28 @@ public class pigLatin1 {
 	public static String pigLatin_v1(String s) {
 		String finish = "";
 		if (firstVowelIndex(s) != -1) {
-			if (isVowel(s.charAt(0))) return s + "way";
 			if(checkCaptial(s)) {
-				System.out.println(" upper");
-
-				char orig = s.charAt(firstVowelIndex(s));
-				char upper = Character.toUpperCase(orig);
-				finish =   upper + s.substring(firstVowelIndex(s)+1) + s.substring(0, firstVowelIndex(s)).toLowerCase() + "ay";
+				if (isVowel(s.toLowerCase().charAt(0))) {
+					char orig = s.charAt(0);
+					char upper = Character.toUpperCase(orig);
+					return upper + s.substring(1) + "way";
+				}
+				if(checkQU(s)) {
+					char orig = s.charAt(firstVowelIndex(s)+1);
+					char upper = Character.toUpperCase(orig);
+					finish = upper + s.substring(firstVowelIndex(s) + 2) + s.substring(0, firstVowelIndex(s)+1).toLowerCase() + "ay";
+				} else {
+					char orig = s.charAt(firstVowelIndex(s));
+					char upper = Character.toUpperCase(orig);
+					System.out.println("no qu");
+					finish = upper + s.substring(firstVowelIndex(s) + 1) + s.substring(0, firstVowelIndex(s)).toLowerCase() + "ay";
+				}
 			} else {
-				return s.substring(firstVowelIndex(s)) + s.substring(0, firstVowelIndex(s)) + "ay";
+				if(checkQU(s)) {
+					return s.substring(firstVowelIndex(s)+1) + s.substring(0, firstVowelIndex(s)+1) + "ay";
+				} else {
+					return s.substring(firstVowelIndex(s)) + s.substring(0, firstVowelIndex(s)) + "ay";
+				}
 			}
 			return finish;
 		}
@@ -45,8 +71,11 @@ public class pigLatin1 {
 
 	public static void main(String[] args) {
 		//System.out.println(pigLatin_v1("hello"));
-		System.out.println(pigLatin_v1("oogway"));
+		System.out.println(pigLatin_v1("Oogway"));
 		System.out.println(pigLatin_v1("pdfs"));
 		System.out.println(pigLatin_v1("Throw"));
+		System.out.println(pigLatin_v1("Question"));
+		System.out.println(pigLatin_v1("question"));
+		System.out.println(pigLatin_v1("squeeze"));
 	}
 }
